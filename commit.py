ce470660407aef59e5b58b631acc47e90679ffd3
x-Error404-x <123807262+x-Error404-x@users.noreply.github.com>
2024-11-29 02:21:45 +0530

    Update commit.py

--- commit.py
+++ commit.py
@@ -5,7 +5,6 @@ from datetime import datetime
 with open("log.txt", "a") as file:
     # 20% chance to skip contributions on some days
     if random.random() < 0.2:
-        file.write(f"No new contribution on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n")
         print("No contributions today!")
     else:
         # Random number of contributions (between 3 and 9)
